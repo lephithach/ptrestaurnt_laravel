2ab8c9338a015a3d5be7f79fe5d966564e40f26f
Phi Thach <jokivadet@gmail.com>
2022-12-08 14:20:52 +0700

    update css

---
+++ resources/views/admin/include/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PT Restaurant</title>
+    @vite(['resources/css/app.css', 'resources/scss/index.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
+</head>
+<body>
+    @include('admin.include.menu-left')
+
+    @yield('root')
+</body>
+</html>
---
+++ resources/views/admin/include/menu-left.blade.php
@@ -0,0 +1,66 @@
+<div id="menu-left" class="bg-sky-800 text-white md:fixed md:top-0 md:bottom-0 md:w-[250px]">
+    <ul class="main-menu">
+        <li class="item active">
+            <a href="./" class="btn-menu">
+                <i class="bi bi-display"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <li class="item" id="nhanvien">
+            <a href="#" class="btn-menu">
+                <i class="bi bi-people-fill"></i>
+                <span>Nhân viên</span>
+                <i class="bi bi-caret-down-fill icon-arrow"></i>
+            </a>
+
+            <div class="sub-menu">
+                <a href="#">Danh sách nhân viên</a>                
+                <a href="#">Chức vụ</a>
+            </div>
+        </li>
+
+        <li class="item">
+            <a href="#" class="btn-menu">
+                <i class="bi bi-cup-straw"></i>
+                <span>Món ăn</span>
+            </a>
+        </li>
+
+        <li class="item" id="donhang">
+            <a href="#" class="btn-menu">
+                <i class="bi bi-journals"></i>
+                <span>Đơn hàng</span>
+                <i class="bi bi-caret-down-fill icon-arrow"></i>
+                <!-- <i class="bi bi-chevron-compact-down icon-arrow"></i> -->
+            </a>
+
+            <div class="sub-menu">
+                <a href="#">Tạo đơn hàng</a>
+                <a href="#">Danh sách đơn hàng Offline</a>
+                <a href="#">Danh sách đơn hàng Online</a>
+            </div>
+        </li>
+
+        <li class="item">
+            <a href="#" class="btn-menu">
+                <i class="bi bi-shield"></i>
+                <span>Góp ý</span>
+            </a>
+        </li>
+
+        <li class="item">
+            <a href="#" class="btn-menu">
+                <i class="bi bi-graph-up"></i>
+                <span>Báo cáo</span>
+            </a>
+        </li>
+
+        <li class="item">
+            <a href="#" class="btn-menu">
+                <i class="bi bi-box-arrow-left"></i>
+                <span>Đăng xuất</span>
+            </a>
+        </li>
+    </ul>
+</div>
